8b3764977664bd413660b64a128f9eba6d3d9303
Jim Stroming <jimstroming@gmail.com>
2016-11-21 10:20:29 -0600

    start writing binary search trees

---
+++ bst.c
@@ -0,0 +1,23 @@
+/* ========================================================================== */
+/*                                                                            */
+/*   Filename.c                                                               */
+/*   (c) 2012 Author                                                          */
+/*                                                                            */
+/*   Description                                                              */
+/*                                                                            */
+/* ========================================================================== */
+
+
+
+typedef struct tree {
+    int item;                /* data item */
+    struct tree *parent;     /* pointer to parent */
+    struct tree *left;       /* pointer to left child */
+    struct tree *right;      /* pointer to right child */
+
+}   tree;
+
+void main()
+{
+
+}
