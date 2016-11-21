e350a92e9df2598b4f7c25619efdccddf38ab03b
Jim Stroming <jimstroming@gmail.com>
2016-11-20 19:51:37 -0600

    keep developing

--- linkedlist.c
+++ linkedlist.c
@@ -26,7 +26,28 @@ void insert_list(list **l, int x)
     *l = p;
 }
 
+list *search_list(list *l, int x)
+{
+    if (l == NULL) return (NULL);
+    
+    if (l->item == x)
+        return(l);
+    else
+        return(search_list(l->next, x));
+}
+
 void main()
 {
-    printf("Hello, World\n"); 
+
+    struct list MyList;
+    MyList.item = 7;
+    MyList.next = NULL;
+    printf("Hello, World\n");
+    list *ListHead = &MyList; 
+    
+    /* Lets try searching the list */
+    list *SevenNode = search_list(ListHead, 7);
+    printf("%p\n", (void *) SevenNode);
+    SevenNode = search_list(ListHead, 6);
+    printf("%p\n", (void *) SevenNode);    
 }
