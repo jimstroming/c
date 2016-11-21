3723081e0e57f195b4cfbd1b563c3a67d5226092
Jim Stroming <jimstroming@gmail.com>
2016-11-21 10:57:53 -0600

    write the insertion routine

--- bst.c
+++ bst.c
@@ -7,7 +7,8 @@
 /*                                                                            */
 /* ========================================================================== */
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct tree {
     int item;                /* data item */
@@ -16,6 +17,27 @@ typedef struct tree {
     struct tree *right;      /* pointer to right child */
 
 }   tree;
+
+
+insert_tree(tree **l, int x, tree *parent)
+{
+    tree *p;                       /* temporary pointer */
+    
+    if (*l == NULL) {
+        p = malloc(sizeof(tree));  /* allocate new node */
+        p->item = x;
+        p->left = p->right = NULL;
+        p->parent = parent;
+        *l = p;                    /* link into parent's record */
+        return;
+    }
+    
+    if (x < (*l)->item)
+        insert_tree(&((*l)->left), x, *l);
+    else
+        insert_tree(&((*l)->right), x, *l);        
+}
+
 
 void main()
 {
