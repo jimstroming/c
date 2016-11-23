6eea0667ddb8bd84f9ccd81a22a6e38afea05e5c
Jim Stroming <jimstroming@gmail.com>
2016-11-23 12:05:27 -0600

    create a hash function

--- hashtable.c
+++ hashtable.c
@@ -9,20 +9,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-long long hashfunction(char *word) 
+unsigned long hashfunction(char *str) 
 {
+    unsigned long hash = 5381;
+    int c;
 
-    return;
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+
 }
 
 
 void main()
 {
-    char a[] = "Hello world";
-    char b[] = "Its me";
-    
+    char a[] = "Hello";
+    char b[] = "world";
+    char c[] = "supercalifragilisticexpealidocious";
+    char d[] = "thelongandwindingroadthatleadstoyourdoorwillneverdisappeariveknowthatroadbefore";
     
     
     printf("%s\n",a);
@@ -33,5 +41,11 @@ void main()
     
     printf("%p\n",(void*)&a);
     printf("%p\n",(void*)&b);
+    
+    
+    printf("%lu\n",hashfunction(a));
+    printf("%lu\n",hashfunction(b));
+    printf("%lu\n",hashfunction(c));
+    printf("%lu\n",hashfunction(d));        
     printf("Done\n");
 }
