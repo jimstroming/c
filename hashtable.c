61dc332c16c2aa12260ebf7b7cf6f2632102e9c5
Jim Stroming <jimstroming@gmail.com>
2016-11-23 09:24:18 -0600

    start writing hash function

--- hashtable.c
+++ hashtable.c
@@ -11,3 +11,27 @@
 #include <stdlib.h>
 
 
+long long hashfunction(char *word) 
+{
+
+    return;
+}
+
+
+void main()
+{
+    char a[] = "Hello world";
+    char b[] = "Its me";
+    
+    
+    
+    printf("%s\n",a);
+    
+    printf("%c\n",b[0]);
+    printf("%c\n",b[5]);
+    printf("%c\n",b[16]);
+    
+    printf("%p\n",(void*)&a);
+    printf("%p\n",(void*)&b);
+    printf("Done\n");
+}
