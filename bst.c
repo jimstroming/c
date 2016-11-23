a05baabcd42d35d9adf5a07786748669d16e28a3
Jim Stroming <jimstroming@gmail.com>
2016-11-22 20:08:38 -0600

    finish the one child case

--- bst.c
+++ bst.c
@@ -106,31 +106,31 @@ delete_tree(tree **l, int x, tree *parent)
             printf("One child case.\n");
             p = *l;            
 
-                if ((*l)->right == NULL){
-                    printf("Left Left case\n");
-                    printf("l is at \n");
-                    printf("%p\n", (void *) &l);   
-                    printf("l points to \n");
-                    printf("%p\n", (void *) l);                     
-                    printf("*l points to \n");
-                    printf("%p\n", (void *) (*l));                    
-                    printf("*l->item is at \n");
-                    printf("%p\n", (void *) &((*l)->item));                    
-                    printf("*l->parent is at \n");
-                    printf("%p\n", (void *) &((*l)->parent));                    
-                    printf("*l->left is at \n");
-                    printf("%p\n", (void *) &((*l)->left));                    
-                    printf("*l->right is at \n");
-                    printf("%p\n", (void *) &((*l)->right));                    
+            if ((*l)->right == NULL){
+                printf("Left Left case\n");
+                printf("l is at \n");
+                printf("%p\n", (void *) &l);   
+                printf("l points to \n");
+                printf("%p\n", (void *) l);                     
+                printf("*l points to \n");
+                printf("%p\n", (void *) (*l));                    
+                printf("*l->item is at \n");
+                printf("%p\n", (void *) &((*l)->item));                    
+                printf("*l->parent is at \n");
+                printf("%p\n", (void *) &((*l)->parent));                    
+                printf("*l->left is at \n");
+                printf("%p\n", (void *) &((*l)->left));                    
+                printf("*l->right is at \n");
+                printf("%p\n", (void *) &((*l)->right));                    
                                                             
-                    ((*l)->left)->parent = parent;
-                    *l = (*l)->left;                 
-                }
-                else{
-                    printf("Left Right case\n");
-                    ((*l)->right)->parent = parent;
-                    *l = (*l)->right;
-                }
+                ((*l)->left)->parent = parent;
+                *l = (*l)->left;                 
+            }
+            else{
+                printf("Left Right case\n");
+                ((*l)->right)->parent = parent;
+                *l = (*l)->right;
+            }
 
 
             
@@ -202,13 +202,16 @@ void main()
     printf("%p\n", (void *) MyTree);     
     
     
-    //delete_tree(&MyTree, 12, NULL);
-    //delete_tree(&MyTree, 0, NULL);
-    //delete_tree(&MyTree, 1, NULL);
-    //delete_tree(&MyTree, 5, NULL);
+    delete_tree(&MyTree, 12, NULL);
+
     printf("Delete node 3\n");
     delete_tree(&MyTree, 3, NULL);
-
+    delete_tree(&MyTree, 14, NULL);
+    delete_tree(&MyTree, 0, NULL);
+    delete_tree(&MyTree, 1, NULL);
+    delete_tree(&MyTree, 2, NULL);    
+    delete_tree(&MyTree, 7, NULL);  
+        
     print_tree(MyTree);
     
     printf("Search for node 9999\n");
